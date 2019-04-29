8c345c5d9f70d3ac018cfd4b8716ab9bc3eb759a
Yavor Dimov <ydimov@gmail.com>
2019-04-29 11:52:56 -0600

    test

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -18,7 +18,7 @@ class SearchViewController: UIViewController {
     
 
     override func viewDidLoad() {
-        //TEST GIT PUSH
+        //TEST GIT PUSH2
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
