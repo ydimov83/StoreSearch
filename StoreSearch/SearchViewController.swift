bd83136e394f08782066365b4bf5e8971d69b146
Yavor Dimov <ydimov@gmail.com>
2019-04-29 11:28:54 -0600

    Chapter 32 complete

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -20,7 +20,6 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
