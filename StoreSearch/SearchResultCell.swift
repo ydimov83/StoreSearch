43d245979edf082938851cc6cab240db32149436
Yavor Dimov <ydimov@gmail.com>
2019-04-30 10:12:19 -0600

    Chapter 33 complete

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,32 @@
+//
+//  StoreSearchCell.swift
+//  StoreSearch
+//
+//  Created by Yavor Dimov on 4/29/19.
+//  Copyright © 2019 Yavor Dimov. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
